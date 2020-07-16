62acc895a4809fdd44eb16d98904f4ac6463e38f
= <=>
2020-07-16 15:36:08 -0700

    Change http request to https

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // allows you to modify the city in the URL
 let city = document.getElementById("city-selection").value;
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=83e4b7f561175c10d89b30e8e704e6cd", function(data) {
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=83e4b7f561175c10d89b30e8e704e6cd", function(data) {
  console.log(data); // console logs API js file, important for reference
 
  //gets icon from weather array
-let icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+let icon = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
 
 // access temp data from file (see the API js file for this stuff, it's also console logged in the browser), round gets rid of decimal units
 let temp = Math.round(data.main.temp);
@@ -25,8 +25,8 @@ $('.temp').append(temp);
 
 function changeCity() {
     city = document.getElementById("city-selection").value;
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=83e4b7f561175c10d89b30e8e704e6cd", function(data) {
-    icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=83e4b7f561175c10d89b30e8e704e6cd", function(data) {
+    icon = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
     temp = Math.round(data.main.temp);
     weather = data.weather[0].main;
     $('.icon').empty();
